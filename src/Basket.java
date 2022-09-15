651c5a1223d6f4f032b53bd89771176d8c498e0a
chmutenkoa <alenachm@gmail.com>
2022-09-15 17:02:53 +0300

    Initial

---
+++ src/Basket.java
@@ -0,0 +1,103 @@
+import java.io.*;
+import java.text.DecimalFormat;
+import java.util.*;
+
+public class Basket {
+    private final String[] products;
+    private final double[] prices;
+    private static Map<Integer, Integer> purchase;
+    private static File textFile;
+
+    public Basket(String[] products, double[] prices) {
+        this.products = products;
+        this.prices = prices;
+        textFile = new File("basket.txt");
+        purchase = new HashMap<>();
+    }
+
+    /**
+     * Method writes the purchases to the text file.
+     *
+     * @throws IOException
+     */
+    private void saveTxt() throws IOException {
+        try (BufferedWriter bw = new BufferedWriter(new FileWriter(textFile))) {
+            purchase.entrySet().stream()
+                    .map(entry -> entry.toString() + "\n")
+                    .forEach(str -> {
+                        try {
+                            bw.write(str);
+                        } catch (IOException e) {
+                            throw new RuntimeException(e);
+                        }
+                    });
+        } catch (IOException e) {
+            System.out.println(e.getMessage());
+        }
+    }
+
+    /**
+     * Restore the shopping list from a text file in which it was previously saved.
+     */
+    protected static void loadFromTxtFile() {
+        if (textFile.exists()) {
+            try (BufferedReader buf = new BufferedReader(new FileReader(textFile))) {
+                String s;
+                while ((s = buf.readLine()) != null) {
+                    String[] read = s.split("(?U)\\W+");
+                    purchase.put(Integer.parseInt(read[0]), Integer.parseInt(read[1]));
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    /**
+     * Add a certain quantity of product to the shopping cart.
+     *
+     * @param productNum
+     * @param amount
+     * @throws IOException
+     */
+    public void addToCart(int productNum, int amount) throws IOException {
+        if (purchase.containsKey(productNum)) {
+            int quantity = purchase.get(productNum) + amount;
+            purchase.put(productNum, quantity);
+        } else {
+            purchase.put(productNum, amount);
+        }
+        saveTxt();
+    }
+
+    /**
+     * Display all purchases, their total cost and quantity (currency decimal format: 0.00).
+     */
+    public void printCart() {
+        StringBuilder sb = new StringBuilder("Ваша корзина:\n");
+        double totalSum = 0;
+        DecimalFormat df = new DecimalFormat("0.00");
+
+        for (Map.Entry<Integer, Integer> entry : purchase.entrySet()) {
+            int productNum = entry.getKey();
+            int quantity = entry.getValue();
+            if (quantity != 0) {
+                double price = prices[productNum];
+                double sum = quantity * price;
+                sb.append(products[productNum]);
+                sb.append(" ");
+                sb.append(quantity);
+                sb.append(" шт ");
+                sb.append(df.format(price));
+                sb.append(" руб/шт ");
+                sb.append(df.format(sum));
+                sb.append(" руб в сумме\n");
+                totalSum += sum;
+            }
+        }
+        sb.append("Итого: ");
+        sb.append(df.format(totalSum));
+        sb.append(" руб");
+        System.out.println(sb);
+    }
+}
---
+++ src/Main.java
@@ -0,0 +1,62 @@
+import java.io.IOException;
+import java.text.DecimalFormat;
+import java.util.Scanner;
+
+/**
+ * 1. Create an object of a product bin, restore a shopping list from the text file
+ * 2. Show a list of products available for purchase;
+ * 3. Scan product number and its quantity from console input;
+ * 4. Store the purchase;
+ * 5. Display all purchases, their total cost and quantity
+ */
+public class Main {
+    public static void main(String[] args) throws IOException {
+        String[] products = {"Молоко", "Хлеб", "Яблоки", "Сыр"};
+        double[] prices = {100.00, 75.00, 110.00, 800.50};
+        Basket basket = new Basket(products, prices);
+        Basket.loadFromTxtFile();
+
+        StringBuilder sb1 = new StringBuilder("Список товаров для покупки: \n");
+        DecimalFormat dfm = new DecimalFormat("0.00");
+        for (int i = 0; i < products.length; i++) {
+            sb1.append(i + 1);
+            sb1.append(". ");
+            sb1.append(products[i]);
+            sb1.append(" ");
+            sb1.append(dfm.format(prices[i]));
+            sb1.append(" руб/шт\n");
+        }
+        System.out.println(sb1);
+
+        Scanner scanner = new Scanner(System.in);
+        int productNum;
+        int productCount;
+        while (true) {
+            System.out.println("Введите через пробел номер товара и количество или введите 'end'");
+            String input = scanner.nextLine();
+            if ("end".equals(input)) {
+                break;
+            }
+            String[] parts = input.split("(?U)\\W+");
+            if (parts.length != 2) {
+                System.out.println("Некорректный ввод данных");
+                continue;
+            }
+            try {
+                productNum = Integer.parseInt(parts[0]) - 1;
+                productCount = Integer.parseInt(parts[1]);
+            } catch (NumberFormatException e) {
+                System.out.println("Следует вводить только числа: сначала - номер товара, затем - пробел и количество");
+                continue;
+            }
+            if (productNum < 0 || productNum > products.length - 1) {
+                System.out.println("Проверьте введенные числа: номер товара");
+                continue;
+            }
+
+            basket.addToCart(productNum, productCount);
+        }
+
+        basket.printCart();
+    }
+}
