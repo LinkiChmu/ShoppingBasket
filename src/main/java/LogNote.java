d8bad93d42c396f69f5abcc7ac0780b25ba6a0e6
chmutenkoa <alenachm@gmail.com>
2022-09-28 11:22:55 +0300

    Class LogNote added, List<int[]> to List<LogNote> in class ClientLog

---
+++ src/main/java/LogNote.java
@@ -0,0 +1,38 @@
+public class LogNote {
+    private int noteID;
+    private int productNum;
+    private int amount;
+
+    public LogNote(int productNum, int amount) {
+        this.productNum = productNum;
+        this.amount = amount;
+    }
+
+    public void displayNote() {
+        System.out.println("Товар номер: " + (productNum + 1) + " - " + amount + " шт.");
+    }
+
+    public int getNoteID() {
+        return noteID;
+    }
+
+    public int getProductNum() {
+        return productNum;
+    }
+
+    public int getAmount() {
+        return amount;
+    }
+
+    public void setNoteID(int noteID) {
+        this.noteID = noteID;
+    }
+
+    public void setProductNum(int productNum) {
+        this.productNum = productNum;
+    }
+
+    public void setAmount(int amount) {
+        this.amount = amount;
+    }
+}
